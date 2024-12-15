f86d07df89180b93f2bcc221b1da732eea0ed2d2
Isvaya <tasyana.pulatova@gmail.com>
2024-12-16 00:01:12 +0500

    fix(extended-2.1): update class according to BEM in js file

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".task-list__button--edit");
-    var containsClass=listItem.classList.contains("edit-mode");
+    var containsClass=listItem.classList.contains("task-list--edit-mode");
     //If class of the parent is .editmode
     if(containsClass){
 
